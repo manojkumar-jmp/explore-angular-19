ce6df96ea92255a6a4bf270952010dc23fbe6e4f
Manoj Kumar <manoj.jmp@gmail.com>
2025-02-15 06:10:34 +0530

    Explore attribute directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { FirstComponent } from './componets/first/first.component';
 import { DataBindingComponent } from './componets/data-binding/data-binding.component';
 import { StructuralDirectivesComponent } from './componets/structural-directives/structural-directives.component';
+import { AttributeDirectivesComponent } from './componets/attribute-directives/attribute-directives.component';
 
 @Component({
   selector: 'app-root',
   imports: [FirstComponent, 
             // DataBindingComponent,
-            StructuralDirectivesComponent],
+           //  StructuralDirectivesComponent,
+            AttributeDirectivesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/componets/attribute-directives/attribute-directives.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-attribute-directives',
+  imports: [CommonModule],
+  templateUrl: './attribute-directives.component.html',
+  styleUrl: './attribute-directives.component.css'
+})
+export class AttributeDirectivesComponent {
+
+}
